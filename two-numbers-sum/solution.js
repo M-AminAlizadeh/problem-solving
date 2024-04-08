8b5c580b705836a2050ef470eb0e9097523cce62
m-aminalizadeh <aminalizadeh@riseup.net>
2024-04-08 10:54:06 +0330

    Update the first solution description

--- two-numbers-sum/solution.js
+++ two-numbers-sum/solution.js
@@ -1,16 +1,31 @@
 const twoNumbersSum = function(nums, target) {
-    // loop inside the nums array
-    // sum each number with the others and compare the result with target (use conditional statement)
-    // if there are two numbers return them as an array else return empty one
-    // try to solve with while loop and for of
-    for (let i = 0; i < nums.length; i++) {
-      for(let j = i + 1; j < nums.length; j++) {
-        if(nums[i] + nums[j] === target){
-          return [nums[i], nums[j]];
+  /* First Solution: solve with 2 loops
+    1. Loop inside the nums array to get each number
+    2. Loop inside the first loop to access other number of the arry
+    3. Use conditional statement (sum each number with the others and compare the result with target)
+    4. Return the result base on the condition
+    ---
+    Time complexity: O(n^2)
+    space complexity: O(n^2)
+  */  
+   for (let i = 0; i < nums.length; i++) {
+     for(let j = i + 1; j < nums.length; j++) {
+       if(nums[i] + nums[j] === target){
+         return [nums[i], nums[j]];
         }
       }
     }
     return [];
+    
+  /* Second Solution: solve with hash table
+    1. 
+    2. 
+    3. 
+    4. 
+    ---
+    Time complexity: O(n)
+    space complexity: O(n)
+  */
 };
 
 module.exports = twoNumbersSum;
